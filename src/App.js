f15eee780fec2bdf5cd8e29789aa9f91f307c60a
Talibiq123 <talibsaeed90@gmail.com>
2023-11-19 17:48:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
+const moviesData = [
+    {name: "The Shawshank Redemption", rating: 9.8, year: 1994},
+    {name: "The Godfather", rating: 9.2, year: 1972},
+    {name: "The Godfather II", rating: 9.0, year: 1974},
+    {name: "The Dark Knight", rating: 9.0, year: 2008},
+    {name: "Taare Jameen Par", rating: 8.4, year: 2007},
+    {name: "Schindler's List", rating: 8.9, year: 1993},
+    {name: "Pulp Fiction", rating: 8.9, year: 1994},
+    {name: "The Lord of the Ring: Return of the King", rating: 8.9, year: 2003},
+    {name: "Jeena Sirf Mere Liye", rating: 4.5, year: 2004},
+]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+
+
+    return (
+        <div>
+            {moviesData.map((movie, index) => (
+                <div key={index}>
+                    <h3>{movie.name}</h3>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
